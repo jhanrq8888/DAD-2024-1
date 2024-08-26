6a6da1498d318c63e9b8debddad8b5bdfeed057e
jhanrq8888 <jhanramos8888@gmail.com>
2024-08-26 07:56:17 -0500

    (:

---
+++ ms-config-server/src/test/java/org/example/msconfigserver/MsConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example.msconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootTest
+class MsConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
